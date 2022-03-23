64eecb2f0620c1ad67ebefa4897526ae3bcc6c21
lyh_linux <lyh2833495@163.com>
2022-03-24 00:02:24 +0800

    add /lib

---
+++ build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lyh/chat_room_libevent/src/redis_subscriber.cpp" "/home/lyh/chat_room_libevent/build/CMakeFiles/client.dir/src/redis_subscriber.cpp.o"
+  "/home/lyh/chat_room_libevent/src/subscriber.cpp" "/home/lyh/chat_room_libevent/build/CMakeFiles/client.dir/src/subscriber.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
